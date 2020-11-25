756da7be06dbe9085f6edd0a3d42d8a15a5a1f55
Krzysztof Klimczak <l2eu@wp.pl>
2020-11-25 11:39:55 +0100

    CreateDatabaseSQLite

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Rocky.Models;
+
+namespace Rocky.Data
+{
+    public class ApplicationDbContext: DbContext{
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+            {
+                
+            }
+            public DbSet<Category> Category { get; set; }
+
+    }
+}

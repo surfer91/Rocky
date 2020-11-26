53e4a8231c1b980e42e85f8b3ec4b18f039ef25e
Krzysztof Klimczak <l2eu@wp.pl>
2020-11-26 12:14:27 +0100

    UpdateFunction

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -50,6 +50,17 @@ namespace Rocky.Controllers
             return View(obj);
         }   
 
+
+      [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {   if (ModelState.IsValid){
+            _db.Category.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");}
+             return View(obj);  
+        }
+
            public IActionResult Delete()
         {
             return View();

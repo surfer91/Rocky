af71b143aab6dd88fe0d775680388ba78c04dfe1
Krzysztof Klimczak <l2eu@wp.pl>
2020-11-26 11:56:53 +0100

    EditView

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -41,5 +41,19 @@ namespace Rocky.Controllers
              return View(obj);  
         }
 
+           public IActionResult Edit(int? id)
+        {   if (id==null||id==0){ return NotFound();}
+            var obj=_db.Category.Find(id);
+            if(obj==null){
+                return NotFound();
+            }
+            return View(obj);
+        }   
+
+           public IActionResult Delete()
+        {
+            return View();
+        }
+
     }
 }

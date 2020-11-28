d76c572bfd0c6fcfc026d21b1b65ce47135a1aea
Krzysztof Klimczak <l2eu@wp.pl>
2020-11-28 11:07:16 +0100

    ViewModelDropdown

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using Rocky.Data;
 using Rocky.Models;
+using Rocky.Models.ViewModels;
 
 namespace Rocky.Controllers
 {
@@ -33,22 +34,34 @@ namespace Rocky.Controllers
 
 
         public IActionResult Upsert(int? id)
-        {   IEnumerable<SelectListItem>CategoryDropDown=_db.Category.Select(i=>new SelectListItem{
-            Text=i.Name,
-            Value=i.Id.ToString()
-            });
+        {  
+            // IEnumerable<SelectListItem>CategoryDropDown=_db.Category.Select(i=>new SelectListItem{
+            // Text=i.Name,
+            // Value=i.Id.ToString()
+            // });
             
-            ViewBag.CategoryDropDown=CategoryDropDown;
+            // ViewBag.CategoryDropDown=CategoryDropDown;
 
-            Product product=new Product();
+            // Product product=new Product();
+
+            ProductVM productVM=new ProductVM(){
+                Product= new Product(),
+                CategorySelectList=_db.Category.Select(i=>new SelectListItem{
+             Text=i.Name,
+             Value=i.Id.ToString()
+             })
+             };
+
+            
+
             if (id==null)
-           { return View(product);}
+           { return View(productVM);}
            else{
-               product=_db.Product.Find(id);
-               if (product==null){
+               productVM.Product=_db.Product.Find(id);
+               if (productVM.Product==null){
                    return NotFound();
                }
-               return View(product);
+               return View(productVM);
 
 
            }
---
+++ Models/ViewModels/ProductVM.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Rocky.Models.ViewModels
+{
+public class ProductVM{
+
+public Product  Product { get; set; }
+public IEnumerable<SelectListItem> CategorySelectList {get;set;}
+
+    }
+}

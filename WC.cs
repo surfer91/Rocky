84c00f2900301b543d6097d863174fb0aa9d91c0
Krzysztof Klimczak <l2eu@wp.pl>
2020-11-30 19:51:04 +0100

    AddNameandPhoneNumbertoRegistration

--- WC.cs
+++ WC.cs
@@ -16,5 +16,7 @@ namespace Rocky
     public static class WC
     {   public static string ImagePath=@"\images\product\";
     public static string SessionCart="ShoppingCartSession";
+    public static string AdminRole="Admin";
+    public static string CustomerRole="Customer";
     }
 }

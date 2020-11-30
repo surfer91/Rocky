533154be3653582702ef19b102b1043a447051a3
Krzysztof Klimczak <l2eu@wp.pl>
2020-11-30 19:38:06 +0100

    ModifyIdentityTable

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace Rocky.Data
             public DbSet<ApplicationType> ApplicationType { get; set; }
 
             public DbSet<Product> Product { get; set; }
+            public DbSet<ApplicationUser> ApplicationUser { get; set; }
 
     }
 }

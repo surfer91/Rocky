f0b3ef3300b0b108acf93dcd23ec5bf64746ca71
Krzysztof Klimczak <l2eu@wp.pl>
2020-12-01 11:07:23 +0100

    CreateCartController

---
+++ Controllers/CartController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Models.ViewModels;
+using Rocky.Utility;
+
+namespace Rocky.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public CartController(ILogger<HomeController> logger,ApplicationDbContext db)
+        {   _db=db;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        { List<ShoppingCart> shoppingCartList=new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart)!=null &&
+        HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count()>0){
+                shoppingCartList=HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+
+        }
+
+        List<int> prodInCart=shoppingCartList.Select(i=>i.ProductId).ToList();
+        IEnumerable<Product> prodList=_db.Product.Where(u=>prodInCart.Contains(u.Id));
+         return View(prodList);
+        }
+            
+        
+
+
+ 
+
+
+          public IActionResult Details(int id)
+        {
+
+             List<ShoppingCart> shoppingCartList=new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart)!=null
+            && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count()>0)
+            {
+                shoppingCartList=HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            DetailsVM DetailsVM=new DetailsVM(){
+            Product=_db.Product.Include(u=>u.Category).Include(u=>u.ApplicationType).Where(u=>u.Id==id).FirstOrDefault(),
+            ExistsInCart=false};
+
+            foreach (var item in shoppingCartList)
+            {
+                if (item.ProductId==id){
+                    DetailsVM.ExistsInCart=true;
+                }
+            }
+    return View(DetailsVM);
+        }
+
+        [HttpPost,ActionName("Details")]
+          public IActionResult DetailsPost(int id)
+        { List<ShoppingCart> shoppingCartList=new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart)!=null
+            && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count()>0)
+            {
+                shoppingCartList=HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+            shoppingCartList.Add(new ShoppingCart{ProductId=id});
+            HttpContext.Session.Set(WC.SessionCart,shoppingCartList);
+            return RedirectToAction(nameof(Index));
+    
+        }
+
+        
+          public IActionResult RemovefromCart(int id)
+        { List<ShoppingCart> shoppingCartList=new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart)!=null
+            && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count()>0)
+            {
+                shoppingCartList=HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            var itemToRemove=shoppingCartList.SingleOrDefault(r=>r.ProductId==id);
+            if (itemToRemove!=null){
+                shoppingCartList.Remove(itemToRemove);
+            }
+
+            
+            HttpContext.Session.Set(WC.SessionCart,shoppingCartList);
+            return RedirectToAction(nameof(Index));
+    
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
